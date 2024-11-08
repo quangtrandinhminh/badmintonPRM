5449069af2c205dca7cb747a7b0d0eec3a08e77d
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-11-08 11:21:16 +0700

    Add slot data

--- Repository/Infrastructure/AppDbContext.cs
+++ Repository/Infrastructure/AppDbContext.cs
@@ -95,5 +95,144 @@ public partial class AppDbContext : DbContext
                 IsActive = true
             }
         );
+
+        // add slot data, 5 slot each yard, from 7:00 to 22:00
+        modelBuilder.Entity<Slot>().HasData(
+            new Slot
+            {
+                SlotId = 1,
+                YardId = 1,
+                Price = 100000,
+                StartTime = new TimeOnly(7, 0),
+                EndTime = new TimeOnly(8, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 2,
+                YardId = 1,
+                Price = 100000,
+                StartTime = new TimeOnly(8, 0),
+                EndTime = new TimeOnly(9, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 3,
+                YardId = 1,
+                Price = 100000,
+                StartTime = new TimeOnly(9, 0),
+                EndTime = new TimeOnly(10, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 4,
+                YardId = 1,
+                Price = 100000,
+                StartTime = new TimeOnly(10, 0),
+                EndTime = new TimeOnly(11, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 5,
+                YardId = 1,
+                Price = 100000,
+                StartTime = new TimeOnly(11, 0),
+                EndTime = new TimeOnly(12, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 6,
+                YardId = 2,
+                Price = 100000,
+                StartTime = new TimeOnly(7, 0),
+                EndTime = new TimeOnly(8, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 7,
+                YardId = 2,
+                Price = 100000,
+                StartTime = new TimeOnly(8, 0),
+                EndTime = new TimeOnly(9, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 8,
+                YardId = 2,
+                Price = 100000,
+                StartTime = new TimeOnly(9, 0),
+                EndTime = new TimeOnly(10, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 9,
+                YardId = 2,
+                Price = 100000,
+                StartTime = new TimeOnly(10, 0),
+                EndTime = new TimeOnly(11, 0),
+                IsActive = true
+                },
+            new Slot
+            {
+                SlotId = 10,
+                YardId = 2,
+                Price = 100000,
+                StartTime = new TimeOnly(11, 0),
+                EndTime = new TimeOnly(12, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 11,
+                YardId = 3,
+                Price = 100000,
+                StartTime = new TimeOnly(7, 0),
+                EndTime = new TimeOnly(8, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 12,
+                YardId = 3,
+                Price = 100000,
+                StartTime = new TimeOnly(8, 0),
+                EndTime = new TimeOnly(9, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 13,
+                YardId = 3,
+                Price = 100000,
+                StartTime = new TimeOnly(9, 0),
+                EndTime = new TimeOnly(10, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 14,
+                YardId = 3,
+                Price = 100000,
+                StartTime = new TimeOnly(10, 0),
+                EndTime = new TimeOnly(11, 0),
+                IsActive = true
+            },
+            new Slot
+            {
+                SlotId = 15,
+                YardId = 3,
+                Price = 100000,
+                StartTime = new TimeOnly(11, 0),
+                EndTime = new TimeOnly(12, 0),
+                IsActive = true
+            }
+       );
     }
 }
---
+++ Repository/Migrations/20241108041909_Add_Slot.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Slot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Slots",
+                columns: new[] { "SlotId", "EndTime", "IsActive", "Price", "StartTime", "YardId" },
+                values: new object[,]
+                {
+                    { 1, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 1 },
+                    { 2, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 1 },
+                    { 3, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 1 },
+                    { 4, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 1 },
+                    { 5, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 1 },
+                    { 6, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 2 },
+                    { 7, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 2 },
+                    { 8, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 2 },
+                    { 9, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 2 },
+                    { 10, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 2 },
+                    { 11, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 3 },
+                    { 12, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 3 },
+                    { 13, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 3 },
+                    { 14, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 3 },
+                    { 15, new TimeOnly(0, 0, 0), true, 100000.0, new TimeOnly(0, 0, 0), 3 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$Y7MreXbBTN1M62isBRpb7euXD7Zo9UswuQQl0fSqKXHGgniZf3lOq");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$11$HQaANF/ibmy.avYSe8R9k.JxHhUWF90hJB6efNZPTthdIkygvMOby");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "Password",
+                value: "$2a$11$l7MsLy8rb7IXyqR8.rT78.a3qenNV0iGnpUD2TUb96Is4VDmbA7qm");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "Password",
+                value: "$2a$11$/WKCi/k7XHwP8xiL7qSoHuPjgqSvoLzoWxwQkmSCqFWhslEFX5S6a");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Slots",
+                keyColumn: "SlotId",
+                keyValue: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$zlO4gMh/wf.cy5o5farxn.uvO7EtlBEcmhgE1KGY1fw98UvBJYwOS");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$11$70cVqqwW26T7NitMn/ezKu.3gefIiaQx1jAAWQGkPbwcusB5DBwIm");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                column: "Password",
+                value: "$2a$11$Jy1XWuMKX2aQKzXzlAv.A.B91md7we6R6T4ePiIxe5CzT5TR98NwW");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 4,
+                column: "Password",
+                value: "$2a$11$M5KCwF7RyyOH2FGIF0Y9oeMLUZQm/68rubyua8LHFDFBcT1JpHXoO");
+        }
+    }
+}
